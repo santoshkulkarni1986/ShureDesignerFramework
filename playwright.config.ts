2dbde5f2fbaa236e323a9e6868aff85b566a109a
Santosh Kulkarni <santoshvqa@gmail.com>
2024-11-05 10:10:11 +0530

    Update playwright.config.ts
    Updated video recording on failure and added the browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,44 +30,48 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    /* Take a screenshot on failure */
+    screenshot: 'only-on-failure',
+
+    /* Record video for each test */
+    video: 'retain-on-failure', // Options: 'off', 'on', 'retain-on-failure', 'retry-with-video'
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] ,headless:false},
+      use: { ...devices['Desktop Chrome'], headless: false },
     },
-/* 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: { ...devices['Desktop Firefox'], headless: false },
     },
-
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    }, */
+      use: { ...devices['Desktop Safari'], headless: false },
+    },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'], headless: false },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'], headless: false },
+    },
 
     /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    {
+      name: 'Microsoft Edge',
+      use: { ...devices['Desktop Edge'], channel: 'msedge', headless: false },
+    },
+    {
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome', headless: false },
+    },
   ],
 
   /* Run your local dev server before starting the tests */
